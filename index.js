99f6f87f5432927b6e8e78f1c656f39fe99f5b94
Newton Tingo <newtontingo@gmail.com>
2024-07-26 20:35:57 +0300

    remove dirt

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const bodyParser = require("body-parser").json;
+const { dbConnection } = require("./config/db");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cookieParser());
+
+app.use(cors(corsOptions));
+app.use(bodyParser());
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+dbConnection().then(() => {
+  server.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+});
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send("FX NEST APIS ARE WORKING PERFECTLY");
+});
+
+const UserRouter = require("./routes/user");
+const ContactUsRouter = require("./routes/contact-us");
+const FileUploadRouter = require("./routes/file-upload");
+
+app.use("/api/user", UserRouter);
+app.use("/api/contact-us", ContactUsRouter);
+app.use("/api/file-upload", FileUploadRouter);
